1888abd441ff4082287bdb7bf8eafaa811d6a670
MasamiTanabe <14ec077@ms.dendai.ac.jp>
2015-10-23 22:34:56 +0900

    test3

---
+++ SumUp2.java
@@ -0,0 +1,21 @@
+package kadai;
+
+import java.util.Scanner;
+
+public class SumUp2 {
+
+	public static void main(String[] args) {
+		Scanner x = new Scanner(System.in);
+		int n=x.nextInt();
+			int i;
+			int a=0;
+			for(i=1;i<=n;i++){
+
+			a=a+i;
+
+
+
+		}
+			System.out.println(a);
+	}
+}
